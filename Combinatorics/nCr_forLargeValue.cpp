5eee74fe9145093c3f2e4c2b7b989366018f5e47
Ali Ashraf Tanvir <137755417+tanvir2021331001@users.noreply.github.com>
2024-12-19 11:48:38 +0600

    Update nCr_forLargeValue.cpp

--- Combinatorics/nCr_forLargeValue.cpp
+++ Combinatorics/nCr_forLargeValue.cpp
@@ -19,3 +19,34 @@ long long modInverse(long long n){
 long long nCr(long long n,long long r){
   return ((f[n]*modInverse(f[r]))%mod*modInverse(f[n-r]))%mod;
 }
+//
+//when n is 1e9 and r <1e5 or near
+long long mod =1e9+7;//prime
+long long pow(int a,int b){
+  if(b<=0){
+    return 1LL;
+  }
+ long long ret=po(a,b/2)%mod;
+  if(b&1){
+    return (((ret*ret)%mod)*a)%mod;
+  }
+  else return (ret*ret)%mod;
+}
+//
+long long ncr(int n, int r) {
+    // nCr is the same as nC(n-r)
+    if (r > n - r) {
+        r = n - r;
+    }
+     long long result = 1;
+    // Calculate the result using the formula:
+    // nCr = n! / (r! * (n-r)!)
+    long long k=1;
+    for (int i = 0; i < r; ++i) {
+        result = (result*(n - i))%mod;
+        k = (k*po((i + 1),mod-2))%mod;
+    }
+    result=(result*k);
+
+    return result%mod;
+}
