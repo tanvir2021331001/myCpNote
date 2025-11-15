d5c2729a4a3c54c0211619fb01d30d6217d41ba5
Ali Ashraf Tanvir <137755417+tanvir2021331001@users.noreply.github.com>
2025-11-15 11:21:39 +0600

    Create linearSeive.cpp

---
+++ NumberTheory/linearSeive.cpp
@@ -0,0 +1,23 @@
+vector<int> linearSieve(int n)
+{
+  vector<int> primes;
+  vector<int> lp(n + 1, 0); // the lowest prime factor
+
+  for (int i = 2; i <= n; i++)
+  {
+    if (lp[i] == 0) 
+    { 
+      lp[i] = i;
+      primes.push_back(i);
+    }
+
+    for (int p : primes)
+    {
+      if (p > lp[i] || i * p > n)
+        break;
+      lp[i * p] = p;
+    }
+  }
+
+  return primes;
+}
