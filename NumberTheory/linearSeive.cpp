7792a6dd1e0abfa6744d724c79849f40b658dc81
Ali Ashraf Tanvir <137755417+tanvir2021331001@users.noreply.github.com>
2025-11-15 11:23:34 +0600

    Update linearSeive.cpp

--- NumberTheory/linearSeive.cpp
+++ NumberTheory/linearSeive.cpp
@@ -1,7 +1,7 @@
 vector<int> linearSieve(int n)
 {
   vector<int> primes;
-  vector<int> lp(n + 1, 0); // the lowest prime factor
+  vector<int> lp(n + 1, 0); //---lowest prime factor lp
 
   for (int i = 2; i <= n; i++)
   {
